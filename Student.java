9e52f23b6538e7d243cefc635e6c4d4dee1e49fe
Jack Li <150150571+0JackLi@users.noreply.github.com>
2023-11-08 08:44:53 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,25 @@
+public class Student
+{
+  private String firstName;
+  private String lastName;
+  private int gradeLevel;
 
+  public Student(String fname, String lname, int gradeLevel)
+  {
+    firstName = fname;
+    lastName = lname;
+    this.gradeLevel = gradeLevel;
+  }
+  public String getFName()
+  {
+    return firstName;
+  }
+  public String getLName()
+  {
+    return lastName;
+  }
+  public int getGrade()
+  {
+    return gradeLevel;
+  }
+}
