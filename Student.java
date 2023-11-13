d4d0208e472bb13b912039004a5780b5b52440d5
Jack Li <150150571+0JackLi@users.noreply.github.com>
2023-11-13 08:45:37 -0500

    Update Student.java
    new file

--- Student.java
+++ Student.java
@@ -3,12 +3,18 @@ public class Student
   private String firstName;
   private String lastName;
   private int gradeLevel;
+  private int height;
 
   public Student(String fname, String lname, int gradeLevel)
   {
     firstName = fname;
     lastName = lname;
     this.gradeLevel = gradeLevel;
+  }
+
+  public Student(int height)
+  {
+    this.height = height;
   }
   public String getFName()
   {
